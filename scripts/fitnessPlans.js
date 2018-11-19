b57c152214ce05f517f7d70164a642ae3766bf68
Michael Sinclair <michaelkevin.sinclair@gmail.com>
2018-11-19 15:40:43 -0800

    updated

--- scripts/fitnessPlans.js
+++ scripts/fitnessPlans.js
@@ -47,3 +47,39 @@ addPlan(true, "How to Build Traps", "weightLoss", null, null);
 function uploadPlans(){
 	
 }
+
+//Update the sections with the user's click
+var isExercise = true;
+var planType = 0;
+
+var exercise = document.getElementById("exercise");
+var meal = document.getElementById("meal");
+
+var type1 = document.getElementById("type1");
+var type2 = document.getElementById("type2");
+var type3 = document.getElementById("type3");
+
+exercise.style.backgroundColor = "yellow";
+
+function switchSection(i){
+	//if the user selects exercise
+	if(i == 0){
+		exercise.style.backgroundColor = "yellow";
+		meal.style.backgroundColor = "transparent";
+		
+		type1.innerHTML = "Upper-body";
+		type2.innerHTML = "Lower-body";
+		type3.innerHTML = "Cardio";
+		
+		isExercise = true;
+	} else {//if the user selects meal
+		exercise.style.backgroundColor = "transparent";
+		meal.style.backgroundColor = "yellow";
+		
+		type1.innerHTML = "Weight-loss";
+		type2.innerHTML = "Bulk-up";
+		type3.innerHTML = "Pre-workout";
+		
+		isExercise = false;
+	}
+}
