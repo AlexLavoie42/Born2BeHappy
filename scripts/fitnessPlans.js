6cd81b14d9c8f69cc1b12bd580c51a0423310da0
Michael Sinclair <michaelkevin.sinclair@gmail.com>
2018-11-19 12:24:51 -0800

    Updated

--- scripts/fitnessPlans.js
+++ scripts/fitnessPlans.js
@@ -1,10 +1,11 @@
 /* This file contains the fitness plan object */
-function FitnessPlan(isExercise, planName, type, description, video){
+function FitnessPlan(isExercise, planName, type, description, video, rating){
 	this.isExercise = isExercise;
 	this.planName = planName;
 	this.type = type;
 	this.description = description;
 	this.video = video;
+	this.rating = rating;
 }
 
 FitnessPlan.getIsExercise = function(){
@@ -27,6 +28,23 @@ FitnessPlan.getVideo = function(){
 	return video;
 }
 
+FitnessPlan.getRating = function(){
+	return rating;
+}
+
 //Add every plan that is stored/added to the fitness page
 var planCount = 0;
 var plan = new Array();
+
+function addPlan(isExercise, planName, type, description, video){
+	plan[planCount] = new FitnessPlan(isExercise, planName, type, description, video);
+	planCount++;
+}
+
+FitnessPlan.getPlanCount = function(){
+	return planCount;
+}
+
+//add default plans
+addPlan(true, "Ryan's One-Month Plan to Strengthen Your Chest", "upperBody", null, null);
+addPlan(true, "How to Build Traps", "weightLoss", null, null);
