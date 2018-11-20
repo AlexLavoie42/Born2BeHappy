1cc5f364cdd1d0532b900bb5eb63c3be43b38980
Alex Lavoie <alexlavoie8@my.bcit.ca>
2018-11-20 09:25:35 -0800

    updated sign in

--- scripts/firebase_setup.js
+++ scripts/firebase_setup.js
@@ -1,9 +1,10 @@
-var config = {
-      apiKey: "AIzaSyApBupnWlvnqBOtM4CnmiUijB17DX6QJFo",
-      authDomain: "born2behappy-17d9c.firebaseapp.com",
-      databaseURL: "https://born2behappy-17d9c.firebaseio.com",
-      projectId: "born2behappy-17d9c",
-      storageBucket: "born2behappy-17d9c.appspot.com",
-      messagingSenderId: "1050819935374"
-};
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyApBupnWlvnqBOtM4CnmiUijB17DX6QJFo",
+    authDomain: "born2behappy-17d9c.firebaseapp.com",
+    databaseURL: "https://born2behappy-17d9c.firebaseio.com",
+    projectId: "born2behappy-17d9c",
+    storageBucket: "born2behappy-17d9c.appspot.com",
+    messagingSenderId: "1050819935374"
+  };
 firebase.initializeApp(config);
