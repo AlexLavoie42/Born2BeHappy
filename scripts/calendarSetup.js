d5cfd2d1f07f77592b60bc00d6478a72bc08ebe8
Alex Lavoie <alexlavoie8@my.bcit.ca>
2018-11-27 09:05:24 -0800

    Started calendar setup js

--- scripts/calendarSetup.js
+++ scripts/calendarSetup.js
@@ -1,8 +1,27 @@
 var db = firebase.firestore();
-var goals = [];
+var goalDoc;
+var goals;
+var days = [];
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (!user) {
     window.location.replace("../sign-in.html");
   }
 });
+
+function selectDay(day){
+  days.push(day);
+}
+
+function saveDays(){
+
+}
+
+loadGoals();
+
+function loadGoals(){
+  goalDoc = db.collection("userGoals").doc(firebase.auth().currentUser.uid);
+  goalDoc.get().then(doc => {
+    goals = doc.data();
+  });
+}
