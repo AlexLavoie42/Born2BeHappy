f0ee72bc93afab3f04e456e2d1fd84c95dc764de
Alex Lavoie <alexlavoie8@my.bcit.ca>
2018-11-27 11:54:39 -0800

    Started calendar setup + commit for michael

--- scripts/calendarSetup.js
+++ scripts/calendarSetup.js
@@ -1,11 +1,14 @@
 var db = firebase.firestore();
 var goalDoc;
+var dayDoc;
 var goals;
-var days = [];
+var days;
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (!user) {
     window.location.replace("../sign-in.html");
+  } else {
+    loadGoals();
   }
 });
 
@@ -17,11 +20,24 @@ function saveDays(){
 
 }
 
-loadGoals();
-
 function loadGoals(){
-  goalDoc = db.collection("userGoals").doc(firebase.auth().currentUser.uid);
-  goalDoc.get().then(doc => {
-    goals = doc.data();
-  });
+  goalDoc = db.collection("userGoals").doc(firebase.auth().currentUser.uid);  
+  dayDoc = db.collection("userDays").doc(firebase.auth().currentUser.uid).collection("goalNum");
+	goalDoc.get().then(function(doc) {
+    if (doc.exists) {
+    goals = Object.values(doc.data());
+    setGoals();
+    } else {
+        console.log("Can't find user DB");
+    }
+ 	}).catch(function(error) {
+ 	    console.log("Error getting document:", error);
+	});
+
+}
+
+function setGoals(){
+  for(i = 0; i < goals.length; i++){
+    
+  }
 }
